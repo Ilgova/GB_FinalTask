66bed73492846e263c633813a45001e990c7d1b8
Ilgova <ilgova.1594@mail.ru>
2023-03-13 22:43:31 +0300

    array creation

--- Final_task/Program.cs
+++ Final_task/Program.cs
@@ -1,1 +1,15 @@
 ﻿// Задача Финальная: Написать программу, которая из массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+
+int n = 9;
+Console.WriteLine($"В заданном массиве всего {n} элементов");
+
+string[] array = new string[n];
+array[0] = "hello";
+array[1] = "2";
+array[2] = "world";
+array[3] = ":-)";
+array[4] = "1234";
+array[5] = "1567";
+array[6] = "Russia";
+array[7] = "Denmark";
+array[8] = "Kazan";
